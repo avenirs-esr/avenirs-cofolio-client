d86ccdd4a1b773f85bfc5132b9260c93687892ea
Pascal Béger <pascal.beger@gmail.com>
2025-05-06 11:51:46 +0200

    revert App.vue changes (made for demo)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-import { AvCard } from './ui';
-
 </script>
 
 <template>
-  <div :style="{padding: '1rem'}">
-  <AvCard width="200px">
-    Hello, this is AvCard's content!
-  </AvCard>
-</div>
   <router-view />
 </template>
