c4ce23650b3d76858039d3443f018d5fd7250d02
Pascal Béger <pascal.beger@gmail.com>
2025-05-07 12:20:00 +0200

    fix: remove dead code

--- src/ui/navigation/types.ts
+++ src/ui/navigation/types.ts
@@ -3,18 +3,3 @@ import type { DsfrNavigationProps } from '@gouvminint/vue-dsfr'
 export interface AvNavigationProps {
   navItems: DsfrNavigationProps['navItems']
 }
-
-export interface AvNavItemProps { label: string, to: string, id?: string, icon?: string }
-
-export interface AvNavProps { label?: string, id?: string }
-
-export interface AvNavMenuProps { title: string, href: string, icon?: string }
-
-export interface NavProps {
-  label?: string
-  id?: string
-}
-
-export interface NavLinkProps { label: string, href: string, id?: string, icon?: string }
-
-export interface NavMenuProps { label: string, href: string, id: string, icon?: string }
