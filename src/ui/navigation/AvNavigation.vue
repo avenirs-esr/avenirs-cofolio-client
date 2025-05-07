a0fbbf4f2f9cfc19143bc74677530569a9663204
Pascal Béger <pascal.beger@gmail.com>
2025-05-07 11:17:59 +0200

    feat: apply colors to hover

--- src/ui/navigation/AvNavigation.vue
+++ src/ui/navigation/AvNavigation.vue
@@ -23,4 +23,21 @@ const props = defineProps<AvNavigationProps>()
 .my-nav-wrapper :deep(.vicon) {
   scale: 0.75;
 }
+
+.my-nav-wrapper :deep(.fr-nav__item > .fr-nav__btn),
+.my-nav-wrapper :deep(.fr-nav__item > .fr-nav__link) {
+  border-radius: 1.5rem 1.5rem 0 0;
+}
+
+.my-nav-wrapper :deep(.fr-nav__item > .fr-nav__btn:hover),
+.my-nav-wrapper :deep(.fr-nav__item > .fr-nav__link:hover) {
+  background-color: var(--dark-background-primary1);
+  color: white;
+}
+
+.my-nav-wrapper :deep(.fr-nav__item > .fr-collapse > .fr-menu__list > .fr-menu__item > .fr-nav__link:hover) {
+  background-color: var(--dark-background-primary1);
+  color: white;
+  border-radius: 0;
+}
 </style>
