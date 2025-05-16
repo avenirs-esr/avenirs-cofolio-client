3990a3ffe6011e21c28e1401a945f7b8d6ba9bb9
Pascal Béger <pascal.beger@gmail.com>
2025-05-16 16:16:43 +0200

    feat(136): StudentOverviewWidget - Replace AvButton with AvCardButton

--- src/ui/cards/AvCard/AvCard.types.ts
+++ src/ui/cards/AvCard/AvCard.types.ts
@@ -1,9 +1,6 @@
-import type { RouteLocationRaw } from 'vue-router'
-
 export interface AvCardProps {
   backgroundColor?: string
   borderColor?: string
   titleBackground?: string
   titleHeight?: string
-  to?: RouteLocationRaw
 }
---
+++ src/ui/interaction/buttons/AvRichButton/AvRichButton.types.ts
@@ -0,0 +1,7 @@
+export interface AvRichButtonProps {
+  iconLeft?: string
+  iconRight?: string
+  borderColor?: string
+  hoverBorderColor?: string
+  onClick: (event: MouseEvent) => void
+}
