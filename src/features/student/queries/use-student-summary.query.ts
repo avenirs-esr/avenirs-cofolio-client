b953be3af0233537fceef79938172946695b9dc1
kelrhourha <karim.elrhourha@gmail.com>
2025-05-19 17:37:51 +0200

    feat(139): add student summary query

---
+++ src/features/student/queries/use-student-summary.query.ts
@@ -0,0 +1,25 @@
+import type { BaseApiException } from '@/common/exceptions/base-api.exception'
+import type { StudentSummaryDTO } from '@/types'
+import profile_banner_placeholder from '@/assets/profile_banner_placeholder.png'
+import profile_picture_placeholder from '@/assets/profile_picture_placeholder.png'
+import { useQuery, type UseQueryReturnType } from '@tanstack/vue-query'
+
+function useStudentSummaryQuery (): UseQueryReturnType<StudentSummaryDTO, BaseApiException> {
+  const queryKey = computed(() => ['user', 'student', 'summary'])
+  return useQuery<StudentSummaryDTO, BaseApiException>({
+    queryKey,
+    // TODO: call /user/{profile}/overview when the endpoint and client are ready
+    queryFn: async (): Promise<StudentSummaryDTO> => {
+      return {
+        id: '123456789',
+        firstname: 'Jeanne',
+        lastname: 'Moulin',
+        profilePicture: profile_picture_placeholder,
+        coverPicture: profile_banner_placeholder,
+        bio: 'Je suis étudiante en chimie et écologie. Passionnée par l’innovation durable, je souhaite utiliser la science pour protéger l’environnement et bâtir un avenir plus respectueux de la planète.'
+      }
+    }
+  })
+}
+
+export { useStudentSummaryQuery }
