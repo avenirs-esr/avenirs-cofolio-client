e350509aefd0cc0587eae2fb493fad6597e3764c
kelrhourha <karim.elrhourha@gmail.com>
2025-06-18 22:52:42 +0200

    build(280): add traces view queries

--- src/common/composables/index.ts
+++ src/common/composables/index.ts
@@ -1,3 +1,4 @@
+export * from './use-infinite-pagination/use-infinite-pagination'
 export * from './use-language-switcher'
 export * from './use-navigation'
 export * from './use-toast/use-toast'
---
+++ src/features/student/queries/use-unassigned-traces.query/use-unassigned-traces.query.ts
@@ -0,0 +1,75 @@
+import type { TracesResponse, TracesViewResponse, TraceViewDTO } from '@/api/avenir-esr'
+import type { BaseApiException } from '@/common/exceptions'
+import type { PaginatedResponse } from '@/types'
+import { defaultGetNextPageParam, useAvInfiniteQuery } from '@/common/composables/use-infinite-pagination/use-infinite-pagination'
+import { PAGE_SIZES, type PageSize } from '@/config'
+import { mockedTracesByPage, mockedTracesByPageSize8, mockedTracesByPageSize12 } from '@/features/student/queries/fixtures'
+import { useQuery, useQueryClient } from '@tanstack/vue-query'
+
+function mapToPaginatedTraces (response: TracesViewResponse): PaginatedResponse<TraceViewDTO[]> {
+  return {
+    data: response.data.traces,
+    page: response.page
+  }
+}
+
+/**
+ * TODO: Remove this function when the API is ready
+ */
+async function getMockedTraces (pageParam: number, pageSize: number): Promise<PaginatedResponse<TracesResponse>> {
+  if (pageSize === PAGE_SIZES[1]) {
+    return mockedTracesByPageSize8[pageParam]
+  }
+  if (pageSize === PAGE_SIZES[2]) {
+    return mockedTracesByPageSize12[pageParam]
+  }
+  return mockedTracesByPage[pageParam]
+}
+
+const commonQueryKeys = ['student', 'unassignedTraces']
+const unassignedTracesCriticalCountQueryKey = [...commonQueryKeys, 'criticalCount']
+
+const TWO_MINUTES = 2 * 60 * 1000
+
+export function useUnassignedTracesQuery (pageSize: PageSize) {
+  const queryClient = useQueryClient()
+
+  return useAvInfiniteQuery<TraceViewDTO[]>({
+    queryKey: computed(() => [...commonQueryKeys, 'list', pageSize]),
+    queryFn: async ({ pageParam = 0 }) => {
+      const response = await getMockedTraces(pageParam, pageSize)
+      /*
+      // TODO: Uncomment when the API is ready
+      const response = await getTracesView({
+        pageSize,
+        page: pageParam,
+        status: GetTracesViewStatus.UNASSOCIATED
+      })
+      */
+      queryClient.setQueryData(
+        unassignedTracesCriticalCountQueryKey,
+        response.data.criticalCount
+      )
+      return mapToPaginatedTraces(response)
+    },
+    getNextPageParam: defaultGetNextPageParam,
+    initialPageParam: 0,
+    staleTime: TWO_MINUTES,
+  })
+}
+
+export function useUnassignedTracesCriticalCount () {
+  const queryClient = useQueryClient()
+
+  const cachedCriticalCount = computed(() => {
+    return queryClient.getQueryData<number>(unassignedTracesCriticalCountQueryKey)
+  })
+
+  return useQuery<number | undefined, BaseApiException, number | undefined>({
+    queryKey: unassignedTracesCriticalCountQueryKey,
+    queryFn: async () => undefined,
+    staleTime: Infinity,
+    enabled: computed(() => cachedCriticalCount.value === undefined),
+    initialData: () => cachedCriticalCount.value
+  })
+}
