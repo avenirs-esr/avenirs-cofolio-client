af7a619297bba99160a2ba136d9c99a5dc287bc1
kelrhourha <karim.elrhourha@gmail.com>
2025-06-20 14:10:36 +0200

    refactor: improve bootstrap test

--- src/bootstrap.test.ts
+++ src/bootstrap.test.ts
@@ -1,17 +1,33 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { createApp } from 'vue'
 
-vi.mock('vue', async (importOriginal) => {
-  const actual = await importOriginal<typeof import('vue')>()
-
+vi.mock('@/plugins/tanstack-query/tanstack-query')
+vi.mock('@/plugins/vue-i18n/vue-i18n')
+vi.mock('@/router')
+vi.mock('@/store')
+vi.mock('./App.vue', () => ({
+  default: {
+    name: 'MockedApp',
+    template: '<div data-testid="mocked-app">Mocked App Component</div>'
+  }
+}))
+vi.mock('vue', async () => {
   return {
-    ...actual,
     createApp: vi.fn(() => ({
       use: vi.fn().mockReturnThis(),
       mount: vi.fn(),
     })),
   }
 })
+
+vi.mock('@gouvfr/dsfr/dist/core/core.main.min.css')
+vi.mock('@gouvfr/dsfr/dist/component/component.main.min.css')
+vi.mock('@gouvfr/dsfr/dist/utility/utility.main.min.css')
+vi.mock('@gouvminint/vue-dsfr/styles')
+vi.mock('@gouvfr/dsfr/dist/scheme/scheme.min.css')
+vi.mock('@gouvfr/dsfr/dist/utility/icons/icons.min.css')
+vi.mock('@/assets/main.css')
+vi.mock('@/ui/styles/main.scss')
 
 const mockCreateApp = vi.mocked(createApp)
 
