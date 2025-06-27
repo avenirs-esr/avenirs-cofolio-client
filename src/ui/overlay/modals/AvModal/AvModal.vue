79157de45486ed1118f4687271d3453ff58b0fac
Pascal Béger <pascal.beger@gmail.com>
2025-06-27 15:40:19 +0200

    refactor(329): update AvModal close button and header

--- src/ui/overlay/modals/AvModal/AvModal.vue
+++ src/ui/overlay/modals/AvModal/AvModal.vue
@@ -1,23 +1,54 @@
 <script setup lang="ts">
 import type { DsfrModalProps } from '@gouvminint/vue-dsfr'
 import type { Slot } from 'vue'
+import { AvButton, type AvButtonProps } from '@/ui/interaction'
+import { MDI_ICONS } from '@/ui/tokens'
+import { useI18n } from 'vue-i18n'
 
-export type AvModalProps = DsfrModalProps
+export type AvModalProps = {
+  closeButtonVariant?: AvButtonProps['variant']
+} & Omit<DsfrModalProps, 'title'>
 
 const props = defineProps<AvModalProps>()
+
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+
 defineSlots<{
   default?: Slot
+  header?: Slot
   footer?: Slot
 }>()
+
+const { t } = useI18n()
+
+const closeButtonLabel = computed(() => props.closeButtonLabel ?? t('global.buttons.close'))
+const closeButtonVariant = computed(() => props.closeButtonVariant ?? 'DEFAULT')
 </script>
 
 <template>
-  <DsfrModal v-bind="props">
+  <DsfrModal
+    v-bind="props"
+    title=""
+  >
     <template #default>
+      <div class="av-modal__header">
+        <slot name="header" />
+      </div>
       <slot />
     </template>
     <template #footer>
-      <slot name="footer" />
+      <div class="footer">
+        <AvButton
+          :icon="MDI_ICONS.CLOSE_CIRCLE_OUTLINE"
+          :label="closeButtonLabel"
+          :variant="closeButtonVariant"
+          size="sm"
+          @click="emit('close')"
+        />
+        <slot name="footer" />
+      </div>
     </template>
   </DsfrModal>
 </template>
@@ -27,5 +58,17 @@ defineSlots<{
 :deep(.fr-modal__body),
 :deep(.fr-modal__footer) {
   background: var(--dialog) !important;
+}
+
+:deep(.fr-btn--close) {
+  display: none !important;
+}
+
+.footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  width: 100%;
+  gap: var(--spacing-sm);
 }
 </style>
