e574b3a30d9a585c92613730e383e1004a30af01
Pascal Béger <pascal.beger@gmail.com>
2025-07-10 11:19:30 +0200

    feat(414): add AvTabs to StudentProjectSkillsView

---
+++ src/features/student/views/StudentProjectSkillsView/components/SkillsViewTabs/SkillsViewTabs.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { AvTab, AvTabs, MDI_ICONS } from '@/ui'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const activeTab = ref(0)
+</script>
+
+<template>
+  <AvTabs v-model="activeTab">
+    <AvTab
+      :title="t('student.views.studentProjectSkillsView.skillsViewTabs.education')"
+      :icon="MDI_ICONS.STARS"
+    >
+      TODO #411 Placeholder...
+    </AvTab>
+    <AvTab
+      :title="t('student.views.studentProjectSkillsView.skillsViewTabs.other')"
+      :icon="MDI_ICONS.STARS"
+    >
+      TODO #416 Placeholder...
+    </AvTab>
+  </AvTabs>
+</template>
+
+<style lang="scss" scoped></style>
