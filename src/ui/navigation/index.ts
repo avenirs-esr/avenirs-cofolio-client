05c223b7c19008baa55179ebd70c61859f11c933
kelrhourha <karim.elrhourha@gmail.com>
2025-07-20 09:35:39 +0200

    feat(443): create AvSideMenu component

--- src/ui/navigation/index.ts
+++ src/ui/navigation/index.ts
@@ -1,3 +1,4 @@
 export * from './AvNavigation'
 export { default as AvPagination } from './AvPagination/AvPagination.vue'
 export * from './AvPagination/utils'
+export { default as AvSideMenu, type AvSideMenuProps } from './AvSideMenu/AvSideMenu.vue'
