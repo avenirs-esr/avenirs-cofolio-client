104b226ed0b6c922e9ac63cd8ad58a0d4d359f3e
Nathan <nathan.pignon79@gmail.com>
2025-09-15 20:14:02 +0200

    fix: update vite config to switch in demo when qualif server is detected

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default ({ mode }: { mode: string }) => {
       __BASE_URL__: JSON.stringify(env.VITE_API_URL || 'http://localhost:3000'),
       __ENABLE_MSW__: JSON.stringify(env.VITE_ENABLE_MSW === 'true'),
       __BEARER_TOKEN__: JSON.stringify(`Bearer ${env.VITE_AVENIR_ESR_ACCESS_TOKEN}` ?? 'Bearer token'),
-      __DEMO_MODE__: JSON.stringify(env.VITE_DEMO_MODE === 'true')
+      __DEMO_MODE__: JSON.stringify(env.VITE_API_URL || 'http://localhost:3000').includes('qualif')
     },
     plugins: [
       vue(),
