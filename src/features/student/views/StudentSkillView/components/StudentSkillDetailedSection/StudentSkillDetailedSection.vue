03f0a5fac2ac61d60742c6c46acab009a6bd0046
Nathan <nathan.pignon79@gmail.com>
2025-10-01 15:40:43 +0200

    feat(650): add title card in skill detailed section

--- src/features/student/views/StudentSkillView/components/StudentSkillDetailedSection/StudentSkillDetailedSection.vue
+++ src/features/student/views/StudentSkillView/components/StudentSkillDetailedSection/StudentSkillDetailedSection.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
+import { AvCard } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
 
+const { skillName } = defineProps<{ skillName: string }>()
+
+const { t } = useI18n()
 </script>
 
 <template>
-  <h3>Skill Detailed Section (placeholder)</h3>
+  <div class="student-skill-detailed-section">
+    <AvCard
+      border-color="var(--other-border-skill-card)"
+      title-background="var(--surface-background)"
+      title-height="4.375rem"
+    >
+      <template #title>
+        <h3>{{ t('student.views.studentSkillView.detailed.title') }}</h3>
+      </template>
+      <template #body>
+        <h3 class="student-skill-detailed-name">
+          {{ skillName }}
+        </h3>
+      </template>
+    </AvCard>
+  </div>
 </template>
 
-<style scoped lang="scss">
-
+<style lang="scss" scoped>
+.student-skill-detailed-section {
+  padding-bottom: 1.5rem;
+}
+.student-skill-detailed-name {
+  padding-top: 1rem;
+  color: var(--dark-background-primary1);
+}
 </style>
