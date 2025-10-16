4e76f69e0cef0c09aef65364b9eee9ee584dde0f
Pascal Béger <pascal.beger@gmail.com>
2025-10-16 14:56:59 +0200

    feat(692): add Toggle component

---
+++ src/common/components/Toggle/Toggle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { AvToggle, type AvToggleProps } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+export interface ToggleProps extends Omit<AvToggleProps, 'activeText' | 'inactiveText'> { }
+
+const props = defineProps<ToggleProps>()
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <AvToggle
+    v-bind="props"
+    :active-text="t('global.avToggle.activeText')"
+    :inactive-text="t('global.avToggle.inactiveText')"
+  />
+</template>
