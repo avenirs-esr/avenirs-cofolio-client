f634cffecab5ed42a85eb02d574fdc6a0af25786
Pascal Béger <pascal.beger@gmail.com>
2025-11-21 09:39:26 +0100

    feat(765): create SelfKnowledgeElementsDropdown

---
+++ src/features/student/selfKnowledge/components/dropdowns/SelfKnowledgeElementsDropdown/SelfKnowledgeElementsDropdown.vue
@@ -0,0 +1,89 @@
+<script lang="ts" setup>
+import { AvDropdown, type AvDropdownItem, MDI_ICONS } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+export interface SelfKnowledgeElementsDropdownProps {
+  addLabel: string
+  deleteLabel: string
+  shareLabel: string
+  isCategoryDeletable: boolean
+}
+
+const { addLabel, deleteLabel, shareLabel, isCategoryDeletable } = defineProps<SelfKnowledgeElementsDropdownProps>()
+
+const emit = defineEmits<{
+  (e: 'addSelected'): void
+  (e: 'deleteSelected'): void
+  (e: 'shareSelected'): void
+  (e: 'deleteCategorySelected'): void
+}>()
+
+const { t } = useI18n()
+
+enum SelfKnowledgeElementsDropdownEvents {
+  ADD = 'add',
+  DELETE = 'delete',
+  SHARE = 'share',
+  DELETE_CATEGORY = 'deleteCategory'
+}
+
+const menuItems = computed<AvDropdownItem[]>(() => {
+  const items = [
+    {
+      name: SelfKnowledgeElementsDropdownEvents.ADD,
+      icon: MDI_ICONS.PLUS_CIRCLE_OUTLINE,
+      label: addLabel
+    },
+    {
+      name: SelfKnowledgeElementsDropdownEvents.DELETE,
+      icon: MDI_ICONS.TRASH_CAN_OUTLINE,
+      label: deleteLabel
+    },
+    {
+      name: SelfKnowledgeElementsDropdownEvents.SHARE,
+      icon: MDI_ICONS.SHARE_VARIANT_OUTLINE,
+      label: shareLabel
+    }
+  ]
+
+  if (isCategoryDeletable) {
+    items.push({
+      name: SelfKnowledgeElementsDropdownEvents.DELETE_CATEGORY,
+      icon: MDI_ICONS.TRASH_CAN_OUTLINE,
+      label: t('student.views.studentProjectTrajectoriesView.selfKnowledge.categoryElementsPaginator.buttons.deleteCategory')
+    })
+  }
+
+  return items
+})
+
+function handleItemSelected (itemName: string) {
+  switch (itemName) {
+    case SelfKnowledgeElementsDropdownEvents.ADD:
+      emit('addSelected')
+      break
+    case SelfKnowledgeElementsDropdownEvents.DELETE:
+      emit('deleteSelected')
+      break
+    case SelfKnowledgeElementsDropdownEvents.SHARE:
+      emit('shareSelected')
+      break
+    case SelfKnowledgeElementsDropdownEvents.DELETE_CATEGORY:
+      emit('deleteCategorySelected')
+      break
+  }
+}
+</script>
+
+<template>
+  <AvDropdown
+    :items="menuItems"
+    :trigger-aria-label="t('global.buttons.moreActions')"
+    :trigger-label="t('global.buttons.moreActions')"
+    width="max-content"
+    @item-selected="handleItemSelected"
+  />
+</template>
+
+<style lang="scss" scoped>
+</style>
