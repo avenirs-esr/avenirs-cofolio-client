a51b39b72232ceafcac3ce440c694e08980930a9
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2025-12-09 10:33:25 +0100

    feat(795): add script to generate feature

---
+++ src/features/student/declaredPrograms/components/interactions/inputs/DeclaredProgramTitleInput/DeclaredProgramTitleInput.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { DECLARED_PROGRAM_TITLE_MAX_LENGTH } from '@/features/student/declaredPrograms/config'
+import { AvInput, type AvInputProps, MDI_ICONS } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+type DeclaredProgramTitleInputProps = Omit<AvInputProps, 'maxlength'>
+
+const {
+  label,
+  prefixIcon,
+  placeholder,
+  ...restProps
+} = defineProps<DeclaredProgramTitleInputProps>()
+
+const modelValue = defineModel<string>()
+const { t } = useI18n()
+
+const avInputProps = computed(() => ({
+  ...restProps,
+  maxlength: DECLARED_PROGRAM_TITLE_MAX_LENGTH,
+  label: label ?? t('student.declaredPrograms.interactions.inputs.DeclaredProgramTitleInput.label'),
+  prefixIcon: prefixIcon ?? MDI_ICONS.ATTACH_FILE,
+  placeholder: placeholder ?? t('student.declaredPrograms.interactions.inputs.DeclaredProgramTitleInput.placeholder')
+}))
+</script>
+
+<template>
+  <AvInput
+    v-bind="avInputProps"
+    v-model="modelValue"
+  />
+</template>
