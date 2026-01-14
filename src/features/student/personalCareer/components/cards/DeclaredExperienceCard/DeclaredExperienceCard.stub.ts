04ba2bd43f36a0483358f38cff71bee722084496
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2026-01-14 14:59:46 +0100

    feat(918): add declared experience card

---
+++ src/features/student/personalCareer/components/cards/DeclaredExperienceCard/DeclaredExperienceCard.stub.ts
@@ -0,0 +1,5 @@
+export const DeclaredExperienceCardStub = defineComponent({
+  name: 'DeclaredExperienceCard',
+  template: '<div class="declared-experience-card-stub" data-testid="declared-experience-card-stub"><slot /></div>',
+  props: ['declaredExperience']
+})
---
+++ src/features/student/personalCareer/components/cards/DeclaredExperienceCard/DeclaredExperienceCard.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import type { DeclaredExperienceViewDTO, EExperienceType } from '@/api/avenir-esr'
+import FloatingIconCard from '@/features/student/global/components/cards/FloatingIconCard/FloatingIconCard.vue'
+import { AvBadge, MDI_ICONS, RI_ICONS } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+defineProps<{ declaredExperience: DeclaredExperienceViewDTO }>()
+
+const { t } = useI18n()
+
+const iconOptions = {
+  name: MDI_ICONS.HUB_OUTLINE,
+  color: 'var(--icon)',
+  right: '0.4rem',
+  bottom: 'calc(-1 * 3.3rem)',
+  borderColor: 'var(--other-border-skill-card)'
+}
+
+const experienceTypeColorMap: Record<EExperienceType, string> = {
+  PROFESSIONAL: 'var(--dark-background-primary3)',
+  PERSONAL: 'var(--dark-background-success)'
+}
+</script>
+
+<template>
+  <FloatingIconCard
+    :title="declaredExperience.title"
+    :icon-options="iconOptions"
+    color="var(--surface-background)"
+    border-color="var(--other-border-skill-card)"
+    border-color-on-hover="var(--dark-background-primary1)"
+    :header-rows="1"
+    title-typography-classes="n6"
+    height="12.8rem"
+  >
+    <template #body>
+      <div class="av-col av-pr-4xl--md av-pt-xl av-pt-none--md">
+        <div class="av-col av-row--md av-align-end av-justify-end--md av-gap-sm">
+          <AvBadge
+            v-if="declaredExperience.experienceType"
+            :label="t(`student.personalCareer.declaredExperienceType.${declaredExperience.experienceType}`)"
+            :background-color="experienceTypeColorMap[declaredExperience.experienceType]"
+            :icon="RI_ICONS.HONOUR_LINE"
+            color="var(--card2)"
+            ellipsis
+          />
+          <AvBadge
+            v-if="declaredExperience.location"
+            :label="declaredExperience.location"
+            :icon="MDI_ICONS.MAP_MARKER_OUTLINE"
+            color="var(--text2)"
+            background-color="transparent"
+            ellipsis
+          />
+        </div>
+      </div>
+    </template>
+  </FloatingIconCard>
+</template>
+
+<style lang="scss" scoped>
+:deep(.n5) {
+  color: var(--text1);
+}
+</style>
