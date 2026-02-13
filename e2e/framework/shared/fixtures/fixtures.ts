d3dd4d844ed4dfb1b902aedab13fce5efd8f7681
Pascal Béger <pascal.beger@gmail.com>
2026-02-13 10:34:09 +0100

    revert(1056): remove activities e2e tests

--- e2e/framework/shared/fixtures/fixtures.ts
+++ e2e/framework/shared/fixtures/fixtures.ts
@@ -1,20 +1,14 @@
 import { setLocaleFromPage } from '@e2e/framework/shared/utils/i18n'
 import { StudentHomePage } from '@e2e/framework/student/home/StudentHomePage'
-import { StudentProjectActivitiesPage } from '@e2e/framework/student/lifeProject/activities/StudentProjectActivitiesPage'
 import { test as base } from 'playwright-bdd'
 
 interface Fixtures {
   studentHomePage: StudentHomePage
-  studentProjectActivitiesPage: StudentProjectActivitiesPage
 }
 
 export const test = base.extend<Fixtures>({
   studentHomePage: async ({ page }, use) => {
     await setLocaleFromPage(page)
     await use(new StudentHomePage(page))
-  },
-  studentProjectActivitiesPage: async ({ page }, use) => {
-    await setLocaleFromPage(page)
-    await use(new StudentProjectActivitiesPage(page))
   }
 })
