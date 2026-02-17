438530cef93aba4918d994ffe258ec4dd9fe3683
Pascal Béger <pascal.beger@gmail.com>
2026-02-17 14:33:08 +0100

    feat(1047): SelfKnowledgeMainSection - add UpdateProfileDrawer

--- src/features/student/selfKnowledge/components/SelfKnowledgeMainSection/SelfKnowledgeMainSection.vue
+++ src/features/student/selfKnowledge/components/SelfKnowledgeMainSection/SelfKnowledgeMainSection.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { useModal } from '@/common/composables'
+import { useDrawer, useModal } from '@/common/composables'
 import SelfKnowledgeCategoryElementsPaginatorCard from '@/features/student/selfKnowledge/components/cards/SelfKnowledgeCategoryElementsPaginatorCard/SelfKnowledgeCategoryElementsPaginatorCard.vue'
 import AddSelfKnowledgeCategoriesModal from '@/features/student/selfKnowledge/components/modals/AddSelfKnowledgeCategoriesModal/AddSelfKnowledgeCategoriesModal.vue'
 import AddSelfKnowledgeCategoryElementDrawer
   from '@/features/student/selfKnowledge/components/overlays/AddSelfKnowledgeCategoryElementDrawer/AddSelfKnowledgeCategoryElementDrawer.vue'
 import { useSelfKnowledgeCategoriesQuery } from '@/features/student/selfKnowledge/queries/self-knowledge.query/self-knowledge.query'
 import { ProfileCard, useStudentSummaryQuery } from '@/features/student/user'
+import UpdateProfileDrawer from '@/features/student/user/components/overlays/UpdateProfileDrawer/UpdateProfileDrawer.vue'
 import { AvButton, AvIconText, MDI_ICONS } from '@avenirs-esr/avenirs-dsav'
 import { useI18n } from 'vue-i18n'
 
@@ -15,7 +16,7 @@ const {
   hideModal: hideAddCategoryModal,
   showModal: showAddCategoryModal
 } = useModal()
-
+const { showDrawer, displayDrawer, hideDrawer } = useDrawer()
 const { categories } = useSelfKnowledgeCategoriesQuery()
 const { data: studentSummary } = useStudentSummaryQuery()
 </script>
@@ -30,6 +31,20 @@ const { data: studentSummary } = useStudentSummaryQuery()
       text-color="var(--title)"
       gap="0.75rem"
     />
+
+    <div
+      v-if="studentSummary"
+      class="av-row av-justify-end"
+    >
+      <AvButton
+        :icon="MDI_ICONS.PENCIL_OUTLINE"
+        :label="t('global.buttons.update')"
+        variant="OUTLINED"
+        small
+        data-testid="display-update-profile-drawer-button"
+        @click="displayDrawer"
+      />
+    </div>
 
     <ProfileCard
       v-if="studentSummary"
@@ -60,4 +75,12 @@ const { data: studentSummary } = useStudentSummaryQuery()
     @confirm="hideAddCategoryModal"
   />
   <AddSelfKnowledgeCategoryElementDrawer />
+
+  <UpdateProfileDrawer
+    v-if="studentSummary"
+    :key="showDrawer ? 'drawer-open' : 'drawer-closed'"
+    :student-summary="studentSummary"
+    :show="showDrawer"
+    :on-close="hideDrawer"
+  />
 </template>
