79fc61373898b0b67ad308fc37f21cccf03f031d
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2026-02-20 17:57:07 +0100

    fix(e2e): add git hook and temp fix for home e2e test

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -2,10 +2,7 @@ import process from 'node:process'
 import * as dotenv from 'dotenv'
 
 export default async function globalSetup () {
-  dotenv.config({
-    path: '.env',
-    override: true
-  })
+  dotenv.config({ path: '.env', override: false })
 
   console.warn('Global setup: Environment loaded')
   console.warn(`Target URL: ${process.env.VITE_API_URL}`)
