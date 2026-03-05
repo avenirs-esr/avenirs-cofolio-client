ebd6bb9a04fea496e58db24a1613e4b6e70a9e94
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2026-03-05 07:15:47 +0100

    fix(1133): fix success message after unsubscription

--- e2e/framework/student/lifeProject/activities/activities.hook.ts
+++ e2e/framework/student/lifeProject/activities/activities.hook.ts
@@ -1,5 +1,5 @@
 import { AfterScenario } from '@e2e/framework/shared/fixtures/fixtures'
 
-AfterScenario({ tags: '@unsubscribe-activity' }, async ({ studentProjectActivitiesPage, studentProjectActivitiesCatalogPage }) => {
+AfterScenario({ tags: '@unsubscribe-activity-from-library' }, async ({ studentProjectActivitiesPage, studentProjectActivitiesCatalogPage }) => {
   await studentProjectActivitiesPage.restoreActivitySubscription(studentProjectActivitiesCatalogPage)
 })
