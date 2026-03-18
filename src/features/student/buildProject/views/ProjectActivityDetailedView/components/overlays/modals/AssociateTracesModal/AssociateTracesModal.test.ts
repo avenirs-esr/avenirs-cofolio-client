3a6351d02c89e9c9d28670142adabdba6f507c9e
Nathan <nathan.pignon79@gmail.com>
2026-03-18 17:56:37 +0100

    feat(1221): create selected associate traces container

--- src/features/student/buildProject/views/ProjectActivityDetailedView/components/overlays/modals/AssociateTracesModal/AssociateTracesModal.test.ts
+++ src/features/student/buildProject/views/ProjectActivityDetailedView/components/overlays/modals/AssociateTracesModal/AssociateTracesModal.test.ts
@@ -5,6 +5,9 @@ import AssociateTracesModal, {
 } from '@/features/student/buildProject/views/ProjectActivityDetailedView/components/overlays/modals/AssociateTracesModal/AssociateTracesModal.vue'
 import { ConfirmAssociateTracesModalStub } from '@/features/student/buildProject/views/ProjectActivityDetailedView/components/overlays/modals/ConfirmAssociateTracesModal/ConfirmAssociateTracesModal.stub'
 import { TracesTypeSelectStub } from '@/features/student/buildProject/views/ProjectActivityDetailedView/components/overlays/TracesTypeSelect/TracesTypeSelect.stub'
+import {
+  SelectedAssociateTracesContainerStub
+} from '@/features/student/buildProject/views/ProjectActivityDetailedView/components/SelectedAssociateTracesContainer/SelectedAssociateTracesContainer.stub'
 import { AvModalStub, BddTest } from '@avenirs-esr/avenirs-dsav/test-utils'
 import { mountComponent } from 'tests/utils'
 import { beforeEach, expect } from 'vitest'
@@ -15,12 +18,25 @@ BddTest().given('an associate traces modal', () => {
   const stubs = {
     AvModal: AvModalStub,
     ConfirmAssociateTracesModal: ConfirmAssociateTracesModalStub,
-    TracesTypeSelect: TracesTypeSelectStub
+    TracesTypeSelect: TracesTypeSelectStub,
+    SelectedAssociateTracesContainer: SelectedAssociateTracesContainerStub
   }
 
   const props: AssociateTracesModalProps = {
     show: true
   }
+
+  const expectedDummyAssociations = [
+    { id: '1', title: '(Placeholder) Trace 1' },
+    { id: '2', title: '(Placeholder) Trace 2' },
+    { id: '3', title: '(Placeholder) Trace 3' },
+    { id: '4', title: '(Placeholder) Trace 4' },
+    { id: '5', title: '(Placeholder) Trace 5' },
+    { id: '6', title: '(Placeholder) Trace 6' },
+    { id: '7', title: '(Placeholder) Trace 7' }
+  ]
+
+  const expectedDummyAssociationsAfterDelete = expectedDummyAssociations.filter(trace => trace.id !== '2')
 
   BddTest().when('the modal is rendered', () => {
     beforeEach(() => {
@@ -58,16 +74,19 @@ BddTest().given('an associate traces modal', () => {
       })
     })
 
+    BddTest().then('it should render the selected associate traces container', () => {
+      const selectedAssociateTracesContainer = wrapper.findComponent(SelectedAssociateTracesContainerStub)
+
+      expect(selectedAssociateTracesContainer.exists()).toBe(true)
+      expect(selectedAssociateTracesContainer.props('traces')).toEqual(expectedDummyAssociations)
+    })
+
     BddTest().then('it should render the confirm associate traces modal hidden by default', () => {
       const confirmModal = wrapper.findComponent(ConfirmAssociateTracesModalStub)
 
       expect(confirmModal.exists()).toBe(true)
       expect(confirmModal.props('show')).toBe(false)
-      expect(confirmModal.props('traces')).toEqual([
-        { id: '1', title: '(Placeholder) Trace 1' },
-        { id: '2', title: '(Placeholder) Trace 2' },
-        { id: '3', title: '(Placeholder) Trace 3' }
-      ])
+      expect(confirmModal.props('traces')).toEqual(expectedDummyAssociations)
     })
 
     BddTest().and('the user changes the selected trace type', () => {
@@ -85,6 +104,26 @@ BddTest().given('an associate traces modal', () => {
         expect(tracesTypeSelect.props('modelValue')).toEqual({
           itemId: EDeclaredActivityAssociationType.TRACE
         })
+      })
+    })
+
+    BddTest().and('the selected associate traces container emits delete event', () => {
+      beforeEach(async () => {
+        const selectedAssociateTracesContainer = wrapper.findComponent(SelectedAssociateTracesContainerStub)
+        selectedAssociateTracesContainer.vm.$emit('delete', '2')
+        await wrapper.vm.$nextTick()
+      })
+
+      BddTest().then('it should remove the deleted trace from the selected traces container', () => {
+        const selectedAssociateTracesContainer = wrapper.findComponent(SelectedAssociateTracesContainerStub)
+
+        expect(selectedAssociateTracesContainer.props('traces')).toEqual(expectedDummyAssociationsAfterDelete)
+      })
+
+      BddTest().then('it should also update the confirm associate traces modal traces', () => {
+        const confirmModal = wrapper.findComponent(ConfirmAssociateTracesModalStub)
+
+        expect(confirmModal.props('traces')).toEqual(expectedDummyAssociationsAfterDelete)
       })
     })
 
