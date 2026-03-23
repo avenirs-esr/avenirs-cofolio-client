6ceac6b4912127f3e2154365ea1744a2b7da6bbe
Pascal Béger <pascal.beger@gmail.com>
2026-03-23 15:02:36 +0100

    feat(1212): create trace associate elements dropdown

---
+++ src/features/student/traces/views/StudentTraceView/components/overlays/dropdowns/TraceAssociateElementsDropdown/TraceAssociateElementsDropdown.stub.ts
@@ -0,0 +1,5 @@
+export const TraceAssociateElementsDropdownStub = defineComponent({
+  name: 'TraceAssociateElementsDropdown',
+  emits: ['activitiesSelected', 'skillsSelected'],
+  template: '<div data-testid="trace-associate-elements-dropdown" />'
+})
