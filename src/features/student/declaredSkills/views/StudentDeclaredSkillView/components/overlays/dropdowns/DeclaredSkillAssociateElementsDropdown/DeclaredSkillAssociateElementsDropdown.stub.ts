98714972dbd89f4dcbf2ba24e79b3d7cfa7f96c4
Nathan <nathan.pignon79@gmail.com>
2026-04-08 15:44:30 +0200

    feat(1366): add associate elements dropdown for activities

---
+++ src/features/student/declaredSkills/views/StudentDeclaredSkillView/components/overlays/dropdowns/DeclaredSkillAssociateElementsDropdown/DeclaredSkillAssociateElementsDropdown.stub.ts
@@ -0,0 +1,5 @@
+export const DeclaredSkillAssociateElementsDropdownStub = defineComponent({
+  name: 'DeclaredSkillAssociateElementsDropdown',
+  emits: ['activitiesSelected'],
+  template: '<div class="declared-skill-associate-elements-dropdown-stub" />'
+})
---
+++ src/features/student/declaredSkills/views/StudentDeclaredSkillView/components/overlays/dropdowns/DeclaredSkillAssociateElementsDropdown/DeclaredSkillAssociateElementsDropdown.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { ICONS } from '@/features/student/global/icons'
+import { AvDropdown } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+const emit = defineEmits<{
+  (e: 'activitiesSelected'): void
+}>()
+
+enum DeclaredSkillAssociateElementsDropdownEvents {
+  ACTIVITIES = 'activities',
+}
+
+const { t } = useI18n()
+
+const menuItems = computed(() => [
+  {
+    name: DeclaredSkillAssociateElementsDropdownEvents.ACTIVITIES,
+    icon: ICONS.ACTIVITY,
+    label: t('student.declaredSkills.views.StudentDeclaredSkillView.DeclaredSkillAssociateElementsDropdown.activities')
+  }
+])
+
+function handleItemSelected (itemName: string) {
+  switch (itemName) {
+    case DeclaredSkillAssociateElementsDropdownEvents.ACTIVITIES:
+      emit('activitiesSelected')
+      break
+  }
+}
+</script>
+
+<template>
+  <AvDropdown
+    data-testid="declared-skill-associate-elements-dropdown"
+    :items="menuItems"
+    :trigger-aria-label="`${t('global.buttons.associate')}...`"
+    :trigger-label="`${t('global.buttons.associate')}...`"
+    :trigger-icon="ICONS.ASSOCIATED"
+    trigger-variant="FLAT"
+    width="max-content"
+    @item-selected="handleItemSelected"
+  />
+</template>
