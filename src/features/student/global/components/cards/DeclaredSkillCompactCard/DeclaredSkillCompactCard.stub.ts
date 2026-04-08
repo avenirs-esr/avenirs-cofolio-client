8442636674939a45b953700a97136bb2fcdd0651
Nathan <nathan.pignon79@gmail.com>
2026-04-08 08:19:12 +0200

    feat(1372): create declared skill compact card component

---
+++ src/features/student/global/components/cards/DeclaredSkillCompactCard/DeclaredSkillCompactCard.stub.ts
@@ -0,0 +1,7 @@
+export const DeclaredSkillCompactCardStub = defineComponent({
+  name: 'DeclaredSkillCompactCard',
+  props: {
+    declaredSkill: Object
+  },
+  template: '<div data-testid="declared-skill-compact-card-stub" />'
+})
