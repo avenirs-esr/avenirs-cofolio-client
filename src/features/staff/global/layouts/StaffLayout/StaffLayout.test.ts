0097a2f7e0790b85e3ce04f959d42651da3c7955
Pascal Béger <pascal.beger@gmail.com>
2026-04-08 09:35:05 +0200

    feat(1398): add footer in staff layout

--- src/features/staff/global/layouts/StaffLayout/StaffLayout.test.ts
+++ src/features/staff/global/layouts/StaffLayout/StaffLayout.test.ts
@@ -1,24 +1,28 @@
 import type { VueWrapper } from '@vue/test-utils'
+import { FooterStub } from '@/common/components/Footer/Footer.stub'
 import { ROUTES } from '@/common/constants'
 import StaffLayout from '@/features/staff/global/layouts/StaffLayout/StaffLayout.vue'
 import { BddTest } from '@avenirs-esr/avenirs-dsav/test-utils'
 import { mountWithRouter } from 'tests/utils'
 import { beforeEach, expect, vi } from 'vitest'
 
+const AvHeaderStub = defineComponent({
+  name: 'AvHeader',
+  props: ['modelValue', 'serviceTitle', 'homeTo', 'quickLinks', 'languageSelector'],
+  emits: ['update:modelValue', 'language-select'],
+  template: `
+    <div>
+      <button @click="quickLinks[0].onClick($event)" data-testid="home-btn">Home</button>
+    </div>
+  `
+})
+
 BddTest().given('a staff layout component', () => {
   let wrapper: VueWrapper<InstanceType<typeof StaffLayout>>
 
   const stubs = {
-    AvHeader: {
-      name: 'AvHeader',
-      props: ['modelValue', 'serviceTitle', 'homeTo', 'quickLinks', 'languageSelector'],
-      emits: ['update:modelValue', 'language-select'],
-      template: `
-      <div>
-        <button @click="quickLinks[0].onClick($event)" data-testid="home-btn">Home</button>
-      </div>
-    `
-    }
+    AvHeader: AvHeaderStub,
+    Footer: FooterStub
   }
 
   beforeEach(async () => {
@@ -30,11 +34,15 @@ BddTest().given('a staff layout component', () => {
 
   BddTest().when('the component is mounted', () => {
     BddTest().then('it should render the AvHeader component', () => {
-      expect(wrapper.findComponent({ name: 'AvHeader' }).exists()).toBe(true)
+      expect(wrapper.findComponent(AvHeaderStub).exists()).toBe(true)
+    })
+
+    BddTest().then('it should render the Footer component', () => {
+      expect(wrapper.findComponent(FooterStub).exists()).toBe(true)
     })
 
     BddTest().then('it should pass correct quickLinks to AvHeader', () => {
-      const header = wrapper.findComponent({ name: 'AvHeader' })
+      const header = wrapper.findComponent(AvHeaderStub)
       const quickLinks = header.props('quickLinks')
 
       expect(quickLinks).toEqual([
@@ -46,25 +54,31 @@ BddTest().given('a staff layout component', () => {
         },
       ])
     })
-  })
 
-  BddTest().when('AvHeader emits update:modelValue', () => {
-    BddTest().then('it should update the searchQuery property', async () => {
-      const avHeader = wrapper.findComponent({ name: 'AvHeader' })
-      avHeader.vm.$emit('update:modelValue', 'test')
-      await wrapper.vm.$nextTick()
+    BddTest().and('AvHeader emits update:modelValue', () => {
+      beforeEach(async () => {
+        const avHeader = wrapper.findComponent(AvHeaderStub)
+        avHeader.vm.$emit('update:modelValue', 'test')
+        await wrapper.vm.$nextTick()
+      })
 
-      expect(wrapper.vm.searchQuery).toBe('test')
+      BddTest().then('it should update the searchQuery property', () => {
+        expect(wrapper.vm.searchQuery).toBe('test')
+      })
     })
-  })
 
-  BddTest().when('searchQuery is updated', () => {
-    BddTest().then('it should pass searchQuery as modelValue to AvHeader', async () => {
-      const avHeader = wrapper.findComponent({ name: 'AvHeader' })
-      wrapper.vm.searchQuery = 'test'
-      await wrapper.vm.$nextTick()
+    BddTest().and('searchQuery is updated', () => {
+      let avHeader: VueWrapper<InstanceType<typeof AvHeaderStub>>
 
-      expect(avHeader.props('modelValue')).toBe('test')
+      beforeEach(async () => {
+        avHeader = wrapper.findComponent(AvHeaderStub)
+        wrapper.vm.searchQuery = 'test'
+        await wrapper.vm.$nextTick()
+      })
+
+      BddTest().then('it should pass searchQuery as modelValue to AvHeader', async () => {
+        expect(avHeader.props('modelValue')).toBe('test')
+      })
     })
   })
 })
