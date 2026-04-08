55e23b218aa775a60aadbc56d7fa506a92b247ee
Nathan <nathan.pignon79@gmail.com>
2026-04-08 08:19:12 +0200

    feat(1372): create declared skill compact card component

---
+++ src/features/student/global/components/cards/DeclaredSkillCompactCard/DeclaredSkillCompactCard.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import type { EExternalSkillType } from '@/api/avenir-esr'
+import type { IdTitle } from '@/types'
+import FloatingIconCard from '@/features/student/global/components/cards/FloatingIconCard/FloatingIconCard.vue'
+import { ICONS } from '@/features/student/global/icons'
+import { AvBadge, type AvBadgeProps, ICONS_DATA_URL } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+export interface DeclaredSkillCompactCardProps {
+  declaredSkill: IdTitle & { type?: EExternalSkillType }
+}
+
+const { declaredSkill } = defineProps<DeclaredSkillCompactCardProps>()
+const { t } = useI18n()
+
+const iconOptions = computed(() => ({
+  name: ICONS.SKILLS,
+  color: 'var(--icon)',
+  bottom: '-2.5rem',
+  borderColor: 'var(--other-border-skill-card)'
+}))
+const typeBadge = computed<AvBadgeProps>(() => ({
+  label: t(`student.declaredSkills.declaredSkillTypes.${declaredSkill.type}`),
+  color: 'var(--text1)',
+  borderColor: 'var(--other-border-skill-card)',
+  backgroundColor: 'var(--surface-background)',
+  icon: ICONS_DATA_URL.MDI_BOOKMARK_CHECK
+}))
+</script>
+
+<template>
+  <FloatingIconCard
+    :title="declaredSkill.title"
+    title-color="var(--text1)"
+    color="var(--light-background-neutral)"
+    :icon-options="iconOptions"
+    border-color="var(--other-border-skill-card)"
+    :header-rows="2"
+    height="7.5rem"
+    custom-title-height="4rem"
+    title-typography-classes="caption-regular"
+  >
+    <template
+      v-if="declaredSkill.type"
+      #body
+    >
+      <div class="av-row">
+        <AvBadge
+          v-bind="typeBadge"
+          small
+          ellipsis
+        />
+      </div>
+    </template>
+  </FloatingIconCard>
+</template>
