0bbacd976339c25cdfcd7075d67715eaf640c08e
ahmeddallali <ahmeddallali560@gmail.com>
2026-05-11 11:33:21 +0200

    feat(1494): create FormFieldCardContainer component #1494
    fix: fix FormFieldCardContainer tests error
    
    Apply suggestion from @p-beger
    
    Co-authored-by: Pascal Béger <119333058+p-beger@users.noreply.github.com>
    
    fix: apply review comments on FormFieldCardContainer
    
    fix: apply other review comments on FormFieldCardContainer

---
+++ src/features/staff/global/components/cards/FormFieldCardContainer/FormFieldCardContainer.stub.ts
@@ -0,0 +1,6 @@
+export const FormFieldCardContainerStub = defineComponent({ name: 'FormFieldCardContainer', props: {
+  orientation: {
+    type: String,
+    required: false
+  }
+}, template: `<div data-testid="form-field-card-container"></div>` })
