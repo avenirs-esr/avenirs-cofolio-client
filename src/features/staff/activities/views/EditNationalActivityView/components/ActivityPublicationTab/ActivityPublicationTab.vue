f96c2726f8c81570a160c33be11f0594b70126b1
Pascal Béger <pascal.beger@gmail.com>
2026-05-21 11:44:53 +0200

    feat(1516): add ActivityPublicationTab

---
+++ src/features/staff/activities/views/EditNationalActivityView/components/ActivityPublicationTab/ActivityPublicationTab.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { ActivityContentDTO } from '@/api/avenir-esr'
+
+interface ActivityContentTabProps {
+  activity: ActivityContentDTO
+}
+
+defineProps<ActivityContentTabProps>()
+</script>
+
+<template>
+  <div class="av-col av-gap-xl">
+    Placeholder...
+  </div>
+</template>
