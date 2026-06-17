0255c2246eb11fb81c6be5513de1906fe2d1be95
ahmeddallali <ahmeddallali560@gmail.com>
2026-06-17 10:39:14 +0200

    feat(1742): create StudentPerspectiveCard

---
+++ src/features/staff/activities/components/cards/StudentPerspectiveCard/StudentPerspectiveCard.stub.ts
@@ -0,0 +1,10 @@
+export const StudentPerspectiveCardStub = defineComponent({
+  name: 'StudentPerspectiveCard',
+  props: {
+    perspective: {
+      type: String,
+      required: false
+    }
+  },
+  template: '<div class="student-perspective-card" />'
+})
