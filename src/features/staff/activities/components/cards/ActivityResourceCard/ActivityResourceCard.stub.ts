c188b7fdbc6b4a4128ef451b05ac4c6b24f3c7bc
syphax-FN <syphaxfennouch@gmail.com>
2026-07-12 12:32:35 +0200

    test(00)

--- src/features/staff/activities/components/cards/ActivityResourceCard/ActivityResourceCard.stub.ts
+++ src/features/staff/activities/components/cards/ActivityResourceCard/ActivityResourceCard.stub.ts
@@ -1,6 +1,8 @@
 import type { ActivityResource } from '@/features/staff/activities/types/resource.types'
 import type { PropType } from 'vue'
 
+
+
 export const ActivityResourceCardStub = defineComponent({
   name: 'ActivityResourceCard',
   props: {
