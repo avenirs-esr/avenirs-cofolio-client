849e3714599d7a3b527c27872a6306268596fc17
Pascal Béger <pascal.beger@gmail.com>
2026-07-16 15:01:46 +0200

    feat(2144): kit - add ValorizedItem

---
+++ src/features/student/kit/views/StudentToolsKitView/components/ValorizedItem/ValorizedItem.vue
@@ -0,0 +1,105 @@
+<script lang="ts" setup>
+import type { Slot } from 'vue'
+import { useGetSelfKnowledgeCategories } from '@/api/avenir-esr'
+import { ROUTES } from '@/common/constants'
+import { ValorizedItemType } from '@/features/student/kit/types/valorized.types'
+import { AvButton, AvTooltip, CUIDA_ICONS } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+export interface ValorizedItemProps {
+  title: string
+  itemId: string
+  parentId?: string
+  type: ValorizedItemType
+  isLastItem?: boolean
+}
+
+const { itemId, parentId = '', type } = defineProps<ValorizedItemProps>()
+
+defineSlots<{
+  default: Slot
+}>()
+
+const { t } = useI18n()
+
+const isSelfKnowledgeItem = computed(() => type.startsWith('SELF_KNOWLEDGE_'))
+
+const { data: selfKnowledgeCategories } = useGetSelfKnowledgeCategories({
+  query: { enabled: isSelfKnowledgeItem }
+})
+
+const to = computed(() => {
+  if (isSelfKnowledgeItem.value && selfKnowledgeCategories.value) {
+    return {
+      name: ROUTES.STUDENT.SELFKNOWLEDGE_CATEGORY.name,
+      params: { id: parentId },
+      query: { elementId: itemId }
+    }
+  }
+
+  switch (type) {
+    case ValorizedItemType.ASSOCIATED_TRACE:
+    case ValorizedItemType.NON_ASSOCIATED_TRACE:
+      return {
+        name: ROUTES.STUDENT.TOOLS_TRACE.name,
+        params: { id: itemId },
+      }
+    case ValorizedItemType.DECLARED_EXPERIENCE:
+      return {
+        name: ROUTES.STUDENT.DECLARED_EXPERIENCE.name,
+        params: { id: itemId },
+      }
+    case ValorizedItemType.DECLARED_PROGRAM:
+      return {
+        name: ROUTES.STUDENT.PERSONAL_CAREER_DECLARED_PROGRAM_DETAILED.name,
+        params: { id: itemId },
+      }
+    case ValorizedItemType.DECLARED_SKILL:
+      return {
+        name: ROUTES.STUDENT.PROJECT_SKILL.name,
+        params: { id: itemId },
+      }
+    default:
+      return undefined
+  }
+})
+</script>
+
+<template>
+  <div
+    class="valorized-item av-col av-gap-xs av-w-full"
+    :class="{ 'valorized-item-last': isLastItem,
+              'av-pb-sm': !isLastItem,
+    }"
+    data-testid="valorized-item"
+  >
+    <div class="av-row av-justify-between av-gap-sm">
+      <AvTooltip :content="title">
+        <span class="title s2-bold av-max-lines">
+          {{ title }}
+        </span>
+      </AvTooltip>
+      <AvButton
+        v-if="to"
+        :label="t('global.buttons.access')"
+        :icon="CUIDA_ICONS.VISIBILITY_ON_OUTLINE"
+        small
+        :to="to"
+        data-testid="access-button"
+      />
+    </div>
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.valorized-item {
+  .title {
+    --max-lines: 1;
+  }
+
+  &:not(.valorized-item-last) {
+    border-bottom: 1px solid var(--stroke);
+  }
+}
+</style>
