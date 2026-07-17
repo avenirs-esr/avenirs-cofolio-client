1981ea4ed15e3dc76114535af8d672de048376f9
iamgreendev <56599296+BanelhaqB@users.noreply.github.com>
2026-07-17 12:33:46 +0200

    feat(2145): add TraceAiProducedBadge component

---
+++ src/features/student/traces/components/badges/TraceAiProducedBadge/TraceAiProducedBadge.stub.ts
@@ -0,0 +1,4 @@
+export const TraceAiProducedBadgeStub = defineComponent({
+  name: 'TraceAiProducedBadge',
+  template: '<div class="trace-ai-produced-badge-stub" />'
+})
---
+++ src/features/student/traces/components/badges/TraceAiProducedBadge/TraceAiProducedBadge.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { AvBadge, MDI_ICONS } from '@avenirs-esr/avenirs-dsav'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <AvBadge
+    :label="t('student.traces.badges.TraceAiProducedBadge.label')"
+    color="var(--contrast-foreground)"
+    background-color="var(--light-foreground-critical)"
+    border-color="transparent"
+    :icon="MDI_ICONS.FLARE"
+    small
+    ellipsis
+    data-testid="trace-ai-produced-badge"
+  />
+</template>
