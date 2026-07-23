a4f95c8980f2c263d44b4afcca0cc6b47b6411d8
ahmeddallali <ahmeddallali560@gmail.com>
2026-07-23 13:56:49 +0200

    feat(2172): create valorizeToggle

---
+++ src/features/student/global/components/interaction/toggles/ValorizeToggle/ValorizeToggle.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { ToggleProps } from '@/common/components/Toggle/Toggle.vue'
+import Toggle from '@/common/components/Toggle/Toggle.vue'
+import { type ComputedRef, useAttrs } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export interface ValorizeToggleProps extends Omit<ToggleProps, 'activeText' | 'inactiveText' | 'statusTextWidth'> { }
+
+const props = defineProps<ValorizeToggleProps>()
+
+const { t } = useI18n()
+const attrs = useAttrs()
+
+const toggleProps: ComputedRef<ToggleProps> = computed(() => ({
+  ...attrs,
+  ...props,
+  activeText: t('student.global.interaction.toggles.ValorizeToggle.activeText'),
+  inactiveText: t('student.global.interaction.toggles.ValorizeToggle.inactiveText'),
+  statusTextWidth: 'fit-content',
+}))
+</script>
+
+<template>
+  <Toggle
+    v-bind="toggleProps"
+  />
+</template>
